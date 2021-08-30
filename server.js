19d56ecb93f1ce35dae0769144bb5bb8b811904e
Vladislav Hanik <vladislavhanik@Mr-Vlado.local>
2021-08-30 15:02:08 +0200

    adding server connection

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.use(cors());
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'vladislavhanik',
-    password : '',
-    database : 'food-addict'
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+    rejectUnauthorized: false
+    }
   }
 });
 
